0c5cb94d52105293d22f3a8bb4604f44cc49c1e0
Seiya <seichan_official@SeiyanoMacBook-Air.local>
2024-11-19 12:00:28 +0900

    make  client

--- client.py
+++ client.py
@@ -1,8 +1,37 @@
 import socket
 import threading
+from datetime import datetime
 
-PORT = 8000
+PORT = 8000 
 BUFFER_SIZE = 1400
+IP = '127.0.0.1' 
 
-def send_video(sock,filename):
+class SocketClient():
     
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        self.socket = None
+
+    def send_video(self,filename):
+      try: 
+         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((self.host, self.port))
+            print('接続されました！')
+    
+            with open(filename,'rb') as video_file:
+            
+                while(chunk := video_file.read(BUFFER_SIZE)):
+                    sock.sendall(chunk)
+                    print(f"{len(chunk)}バイト送信されました")
+        
+         print("動画の送信を完了しました") 
+        
+      except Exception as e:
+          
+          print(f"エラーが発生しました:{e}") # エラーが発生した場合の処理
+            
+    
+    
+                
+                
